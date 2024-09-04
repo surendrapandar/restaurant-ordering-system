b7ff73e4f0e86104959ad57196a645c15909eacf
surendrachoudhary <surendrachoudhary2976@gmail.com>
2024-09-04 19:50:30 +0530

    testing app with console

--- restaurant-ordering-system/src/firebase/firebaseConfig.js
+++ restaurant-ordering-system/src/firebase/firebaseConfig.js
@@ -2,6 +2,8 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
